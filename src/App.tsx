51ba971a8234a6ddf9505f5463d29314f11aa9db
Sanghyun Park <shkrmkr36@gmail.com>
2021-03-31 04:25:52 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div>
+      <h1>
+        <code>hello world </code>
+        <span role="img" aria-label="emoji-hand">
+          👋👋
+        </span>
+      </h1>
+    </div>
+  );
+};
